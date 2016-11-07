7bfa55850f5c36803b7201f85581cd11f17fe23a
Justin <justin@drawboard.com>
2016-11-08 09:23:11 +1100

    Placeholders.

---
+++ PanAndZoomWithInteractionTracker/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Hosting;
+
+namespace PanAndZoomWithInteractionTracker
+{
+    public static class Extensions
+    {
+        public static Visual Visual(this UIElement element)
+        {
+            return ElementCompositionPreview.GetElementVisual(element);
+        }
+
+        public static ContainerVisual ContainerVisual(this UIElement element)
+        {
+            var hostVisual = ElementCompositionPreview.GetElementVisual(element);
+            var root = hostVisual.Compositor.CreateContainerVisual();
+            ElementCompositionPreview.SetElementChildVisual(element, root);
+            return root;
+        }
+    }
+}
--- PanAndZoomWithInteractionTracker/MainPage.xaml.cs
+++ PanAndZoomWithInteractionTracker/MainPage.xaml.cs
@@ -1,12 +1,15 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Numerics;
 using Windows.Devices.Input;
+using Windows.UI;
 using Windows.UI.Composition;
 using Windows.UI.Composition.Interactions;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Shapes;
 
 namespace PanAndZoomWithInteractionTracker
@@ -22,7 +25,9 @@ namespace PanAndZoomWithInteractionTracker
         private VisualInteractionSource _interactionSource;
         private InteractionTracker _tracker;
 
-        private float _lastScale;
+        private float _newTrackerScale;
+
+        private static readonly Random Random = new Random();
 
         #endregion
 
@@ -47,13 +52,15 @@ namespace PanAndZoomWithInteractionTracker
 
         private void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
-            _rootVisual = ElementCompositionPreview.GetElementVisual(Root);
+            _rootVisual = Root.Visual();
             _rootVisual.Size = new Vector2((float)Root.ActualWidth, (float)Root.ActualHeight);
 
-            _pathContainerVisual = ElementCompositionPreview.GetElementVisual(PathContainer);
+            _pathContainerVisual = PathContainer.Visual();
             _pathContainerVisual.Size = _rootVisual.Size;
 
             _compositor = _rootVisual.Compositor;
+
+            //CreateMoreShapes();
 
             _interactionSource = VisualInteractionSource.Create(_rootVisual);
             _interactionSource.PositionXSourceMode = InteractionSourceMode.EnabledWithInertia;
@@ -65,8 +72,8 @@ namespace PanAndZoomWithInteractionTracker
 
             _tracker = InteractionTracker.CreateWithOwner(_compositor, this);
             _tracker.InteractionSources.Add(_interactionSource);
-            _tracker.MaxPosition = new Vector3(_rootVisual.Size, 0.0f) * 10.0f;
-            _tracker.MinPosition = _tracker.MaxPosition * -1.0f;
+            _tracker.MaxPosition = new Vector3(_rootVisual.Size, 0.0f) * 3.0f;
+            _tracker.MinPosition = _tracker.MaxPosition * 1.0f;
             _tracker.MinScale = 0.9f;
             _tracker.MaxScale = 12.0f;
             _tracker.ScaleInertiaDecayRate = 0.96f;
@@ -131,7 +138,7 @@ namespace PanAndZoomWithInteractionTracker
             //Debug.WriteLine($"Scale: {args.Scale}");
             //Debug.WriteLine($"Position: {args.Position}");
 
-            _lastScale = args.Scale;
+            _newTrackerScale = args.Scale;
         }
 
         public void InertiaStateEntered(InteractionTracker sender, InteractionTrackerInertiaStateEnteredArgs args)
@@ -141,6 +148,35 @@ namespace PanAndZoomWithInteractionTracker
         public void IdleStateEntered(InteractionTracker sender, InteractionTrackerIdleStateEnteredArgs args)
         {
             Debug.WriteLine("Fingers up");
+
+            return;
+
+            foreach (var child in PathContainer.Children)
+            {
+                var shape = child as Path;
+                if (shape == null) return;
+
+                // TODO: ALso need to check if the shape is IN VIEW.
+
+                var originalWidth = shape.ActualWidth;
+                var originalHeight = shape.ActualHeight;
+
+                var shapeVisual = shape.Visual();
+                var currentScale = shapeVisual.Scale.X;
+
+                // When scaling up, we first scale down the shape, and then increase their Width and Height so
+                // they will be crystal sharp.
+                if (_newTrackerScale > currentScale)
+                {
+                    var downScale = currentScale / _newTrackerScale;
+
+                    shapeVisual.Scale = new Vector3(downScale, downScale, 0.0f);
+                    Debug.WriteLine($"Updated scale: {shapeVisual.Scale}");
+
+                    shape.Height = originalHeight * _newTrackerScale;
+                    Debug.WriteLine($"New Width: {shape.Width}");
+                }
+            }
         }
 
         public void CustomAnimationStateEntered(InteractionTracker sender, InteractionTrackerCustomAnimationStateEnteredArgs args)
@@ -149,6 +185,33 @@ namespace PanAndZoomWithInteractionTracker
 
         public void RequestIgnored(InteractionTracker sender, InteractionTrackerRequestIgnoredArgs args)
         {
+        }
+
+        #endregion
+
+        #region Methods
+        public static int GetRandomNumber(int min, int max) => Random.Next(min, max);
+
+        private void CreateMoreShapes(int numberOfShapesToCreate = 10000)
+        {
+            for (var i = 0; i < numberOfShapesToCreate; i++)
+            {
+                var size = GetRandomNumber(4, 32);
+                var x = GetRandomNumber(4, (int)_pathContainerVisual.Size.X);
+                var y = GetRandomNumber(4, (int)_pathContainerVisual.Size.Y);
+
+                var shape = new Rectangle
+                {
+                    Fill = new SolidColorBrush(Color.FromArgb(180, (byte)Random.Next(255), (byte)Random.Next(255), (byte)Random.Next(255))),
+                    Width = size,
+                    Height = size
+                };
+
+                Canvas.SetLeft(shape, x);
+                Canvas.SetTop(shape, y);
+
+                PathContainer.Children.Add(shape);
+            }
         }
 
         #endregion
